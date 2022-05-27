00f2790752a9a8cee3f0c8c5a2178e91a4251006
byernrl9 <12na0ki8@gmail.com>
2022-05-27 17:25:19 +0900

    投稿画面の編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,8 +7,8 @@ class Item < ApplicationRecord
   validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_day_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
 
-  with_options presence: true, format: { :with /\A[0-9]+\z/ } do
-  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999} }
+  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999} 
   end
 
   belongs_to :user
@@ -24,6 +24,4 @@ class Item < ApplicationRecord
 
   # has_one :purchase_records
   
-
-
-end 
+end
