95c3002e2363be5391c0d4f63970257e65baaf1c
byernrl9 <12na0ki8@gmail.com>
2022-05-27 16:30:28 +0900

    ActiveHashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,11 @@
 class Item < ApplicationRecord
   validates :name, presence: true
   validates :item_explanation, presence: true
-  validates :category_id, presence: true
-  validates :item_condition_id, presence: true
-  validates :delivery_fee_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :shipping_day_id, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_condition_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_fee_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_day_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
 
   with_options presence: true, format: { :with /\A[0-9]+\z/ } do
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999} }
@@ -13,7 +13,17 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_condition
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :shipping_day
+
+
   # has_one :purchase_records
+  
 
 
 end 
