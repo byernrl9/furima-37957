25c14c4671c469d645afc045c8bd59a7d693df3b
byernrl9 <12na0ki8@gmail.com>
2022-06-08 19:32:25 +0900

    非同期通信を行うまでの実装

---
+++ app/javascript/tag.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const tagNameInput = document.querySelector("#item_form_tag_name");
+  if (tagNameInput){
+    const inputElement = document.getElementById("item_form_tag_name");
+    inputElement.addEventListener("input", () => {
+      const keyword = document.getElementById("item_form_tag_name").value;
+      console.log(keyword);
+      const XHR = new XMLHttpRequest();
+      XHR.open("GET", `/items/search/?keyword=${keyword}`, true);
+      XHR.responseType = "json";
+      XHR.send();
+      
+    });
+  };
+});
